3566d550a1fe041485a7b9aa8ba291a86a20bdce
abdeyali <abdeali535353@gmail.com>
2026-04-03 16:44:39 +0530

    Fix: handle AnonymousUser role error in permissions

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Finance Backend API is running 🚀"
+    })
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
--- core/permissions.py
+++ core/permissions.py
@@ -2,18 +2,27 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'admin'
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return getattr(request.user, "role", None) == "admin"
 
 
 class IsAnalystOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role in ['analyst', 'admin']
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return getattr(request.user, "role", None) in ["analyst", "admin"]
 
 
 class IsViewer(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'viewer'
-    
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return getattr(request.user, "role", None) == "viewer"
+
+
 class IsViewerOrAbove(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role in ['viewer', 'analyst', 'admin']
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return getattr(request.user, "role", None) in ["viewer", "analyst", "admin"]
